b26d47cc38aa396af6792ed6c1b6e4ece9e3621c
1a2s2s3d <1a2s2s3d@gmail.com>
2022-03-25 02:35:00 +0900

    0325

--- app/src/main/java/com/example/todolist/AddTodoActivity.kt
+++ app/src/main/java/com/example/todolist/AddTodoActivity.kt
@@ -2,10 +2,47 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityAddTodoBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.ToDoDao
 
 class AddTodoActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAddTodoBinding
+    lateinit var db : AppDatabase
+    lateinit var todoDao : ToDoDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_todo)
+
+        binding = ActivityAddTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        binding.btnCompletion.setOnClickListener{
+            insertTodo()
+        }
+    }
+
+    private fun insertTodo(){
+        val todoTitle = binding.edtTitle.text.toString()
+        var todoImportance = binding.radioGroup.checkedRadioButtonId
+
+        when(todoImportance){
+            R.id.btn_high ->{
+                todoImportance = 1
+            }
+            R.id.btn_middle ->{
+                todoImportance = 2
+            }
+            R.id.btn_low ->{
+                todoImportance = 3
+            }
+            else -> {
+                todoImportance = -1
+            }
+        }
     }
 }
